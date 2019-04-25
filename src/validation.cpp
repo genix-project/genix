51c29b61aed6a06b79bdfffdbe11d929f53999e1
farsider350 <31876349+farsider350@users.noreply.github.com>
2019-04-25 14:25:30 +1000

    BPT-MN payouts pre 882

--- src/validation.cpp
+++ src/validation.cpp
@@ -1293,7 +1293,7 @@ CAmount GetBlockSubsidy(int nPrevBits, int nPrevHeight, const Consensus::Params&
 CAmount GetMasternodePayment(int nHeight, CAmount blockValue)
 {
     // Until tiers implementation, MN gets 60% from block reward
-    if (nHeight > 100 && nHeight < 2882) {
+    if (nHeight > 100 && nHeight < 882) {
 	return blockValue * 0.2;
 	}
     return blockValue * 0.6;
