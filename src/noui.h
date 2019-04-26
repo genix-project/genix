a834ee8a69af91f7d368e0d0de2bf34658ef732d
farsider350 <31876349+farsider350@users.noreply.github.com>
2019-04-26 20:39:49 +1000

    Governance
    Testnet for Decentralized Governance by Blockchain (DGBB)
    Budget Proposals, Voting, Funding and Superblocks
    30sec Blocks
    First Halving Block 1000
    All BIPs active within first 1000 blocks
    Budget and Superblocks activate Block 1001
    Proposal establishment time - 2 hrs
    Payments Cycle every 200 blocks

---
+++ src/noui.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2013 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef GENIX_NOUI_H
+#define GENIX_NOUI_H
+
+extern void noui_connect();
+
+#endif // GENIX_NOUI_H
