36e58889a0dd996d4877970124d668aa383fedbb
farsider350 <31876349+farsider350@users.noreply.github.com>
2019-05-30 23:02:11 +1000

    Update Depends

--- depends/packages/native_cdrkit.mk
+++ depends/packages/native_cdrkit.mk
@@ -22,5 +22,5 @@ define $(package)_stage_cmds
 endef
 
 define $(package)_postprocess_cmds
-  rm bin/igenixfy bin/isoinfo bin/isodump bin/isodebug bin/devdump
+  rm bin/isovfy bin/isoinfo bin/isodump bin/isodebug bin/devdump
 endef
