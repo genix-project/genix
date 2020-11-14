e3ed67801c4888cc02ecfb01d3d8891ac2e2e754
KMSoftware <thelimevodka@gmail.com>
2020-11-14 07:10:51 -0500

    Fix overview page (#14)
    * dkgInterval & dkgMiningWindowEnd increased by 6
    
    * UI remove Private Send
    
    * Remove private send startup & rpc commands.
    Disable privatesend client start up.
    
    * Fix file permissions on repository.
    
    * Remove ps qt option.
    
    * Main net preparations.  (#9)
    
    * dkgInterval & dkgMiningWindowEnd increased by 6
    
    * UI remove Private Send
    
    * Remove private send startup & rpc commands.
    Disable privatesend client start up.
    
    * Fix file permissions on repository.
    
    * Remove ps qt option.
    
    Co-authored-by: farsider350 <farsider350@github.com>
    
    * Revert "Remove private send startup & rpc commands."
    
    This reverts commit 85fc42da1a2fb6d5fb73a2c2a9b83fdac187238d.
    
    * Revert "UI remove Private Send"
    
    This reverts commit 7f9a3fc182d8dd10ce84b747fc3203128d5fe621.
    
    * Private send removal.
    
    * Fix debug console button.
    
    * Adjust some "genix" casing.
    
    * re-apply overview page fix.
    
    * update .gitignore to execlude vscode/settings.json
    
    * Revert "UI remove Private Send"
    
    This reverts commit 7f9a3fc182d8dd10ce84b747fc3203128d5fe621.
    
    * Fix debug console button.
    
    Co-authored-by: farsider350 <farsider350@github.com>

--- src/qt/sendcoinsdialog.h
+++ src/qt/sendcoinsdialog.h
@@ -53,7 +53,7 @@ public Q_SLOTS:
     void accept();
     SendCoinsEntry *addEntry();
     void updateTabsAndLabels();
-    void setBalance(const CAmount& balance, const CAmount& unconfirmedBalance, const CAmount& immatureBalance,
+    void setBalance(const CAmount& balance, const CAmount& unconfirmedBalance, const CAmount& immatureBalance, const CAmount& anonymizedBalance,
                     const CAmount& watchOnlyBalance, const CAmount& watchUnconfBalance, const CAmount& watchImmatureBalance);
 
 private:
